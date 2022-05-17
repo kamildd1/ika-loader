f80bfd94d0465d3dd2d64a458fce6296f451869c
Mateusz <mateusz.zaranek123@gmail.com>
2022-05-17 12:28:00 +0200

    Add Mock Unit test DataStorage

--- src/test/java/com/ikea/loader/LoaderApplicationTests.java
+++ src/test/java/com/ikea/loader/LoaderApplicationTests.java
@@ -1,13 +1,36 @@
 package com.ikea.loader;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class LoaderApplicationTests {
+import com.ikea.loader.model.Player;
+import com.ikea.loader.service.storage.CompetitionStorageRepository;
+import com.ikea.loader.service.storage.DataStorageImpl;
+import com.ikea.loader.service.storage.DataStorageRepository;
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import java.util.List;
+
+@RunWith(MockitoJUnitRunner.class)
+public class LoaderApplicationTests {
+
+	@InjectMocks
+	public DataStorageImpl dataStorage;
+
+	@Mock
+	private DataStorageRepository dataStorageRepository;
+
+	@Mock
+	private CompetitionStorageRepository competitionStorageRepository;
 
 	@Test
-	void contextLoads() {
+	public void checkPlayers(){
+		//given
+		//when
+		List<Player> playerList= dataStorage.getAllDataPlayer();
+		//then
+		Assertions.assertNotNull(playerList);
 	}
-
 }
